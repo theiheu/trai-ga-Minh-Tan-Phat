b4e06bcccb1e0d51e5a9165acad4e3c7aafe3602
The Hieu <thehieu236@gmail.com>
2023-12-28 16:12:58 +0700

    Refactor blog component layout and add image and link

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -9,26 +9,49 @@ const Blogs = () => {
           <div className="h-1 w-20 bg-yellow-500 rounded" />
         </div>
       </div>
-      <div className="px-5">
+      <div className="">
         <div className="-my-8 divide-y-2 divide-gray-100">
           <div className="py-8 flex flex-wrap md:flex-nowrap">
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font text-gray-700">
-                CATEGORY
-              </span>
-              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col pr-5">
+              <img
+                src="src/assets/mh_giacam_binhduong3.jpg"
+                alt=""
+                className="rounded-md "
+              />
+              <div className="flex my-2">
+                <img
+                  src="public/img/vukehoach.png"
+                  alt=""
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col ml-2">
+                  <span className="font-semibold title-font text-gray-700">
+                    Vụ kế hoạch
+                  </span>
+                  <span className="mt-1 text-gray-500 text-sm">
+                    10/10/2018 | 2:18:00 PM
+                  </span>
+                </div>
+              </div>
             </div>
+
             <div className="md:flex-grow">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                Bitters hashtag waistcoat fashion axe chia unicorn
+                Mô hình Trại gia cầm ứng dụng công nghệ cao tại tỉnh Bình Dương
               </h2>
               <p className="leading-relaxed">
-                Glossier echo park pug, church-key sartorial biodiesel
-                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-                party messenger bag selfies, poke vaporware kombucha
-                lumbersexual pork belly polaroid hoodie portland craft beer.
+                Trại gia cầm Trại gia cầm của ông Lê Văn Dương (trại gia cầm
+                Minh Tân Phát) có tổng diện tích 3 ha được đầu tư xây dựng tại
+                xã Minh Tân, huyện Dầu Tiếng, tỉnh Bình Dương với tổng đàn
+                240.000 con gà đẻ, cung ứng ra thị trường gần 220.000 quả trứng
+                mỗi ngày.
               </p>
-              <a className="text-yellow-500 inline-flex items-center mt-4">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://vukehoach.mard.gov.vn/Webcontent/MoHinh.aspx?ID=32"
+                className="text-yellow-500 inline-flex items-center mt-4"
+              >
                 Đọc thêm
                 <svg
                   className="w-4 h-4 ml-2"
@@ -46,57 +69,46 @@ const Blogs = () => {
             </div>
           </div>
           <div className="py-8 flex flex-wrap md:flex-nowrap">
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font text-gray-700">
-                CATEGORY
-              </span>
-              <span className="mt-1 text-gray-500 text-sm">12 Jun 2019</span>
+            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col pr-5">
+              <img
+                src="public/img/t7vevanduong.jpg"
+                alt=""
+                className="rounded-md "
+              />
+              <div className="flex my-2">
+                <img
+                  src="public/img/baobinhduong.png"
+                  alt=""
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="flex flex-col ml-2">
+                  <span className="font-semibold title-font text-gray-700">
+                    Báo bình dương
+                  </span>
+                  <span className="mt-1 text-gray-500 text-sm">
+                    07/10/2015 | 08:29:47 AM
+                  </span>
+                </div>
+              </div>
             </div>
+
             <div className="md:flex-grow">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                Meditation bushwick direct trade taxidermy shaman
+                Lê Văn Dương: Kỹ sư xây dựng làm… nông nghiệp công nghệ cao
               </h2>
               <p className="leading-relaxed">
-                Glossier echo park pug, church-key sartorial biodiesel
-                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-                party messenger bag selfies, poke vaporware kombucha
-                lumbersexual pork belly polaroid hoodie portland craft beer.
+                Thành công trên đất mới Bình Dương với nghề cơ khí, xây dựng nhà
+                xưởng, đột nhiên, anh Lê Văn Dương (phường Lái Thiêu, TX.Thuận
+                An) lại rẽ bước kinh doanh sang một ngành nghề mới lạ, quyết chí
+                làm nông nghiệp công nghệ cao tại xã Minh Tân, huyện Dầu Tiếng.
+                Quyết định có phần mạo hiểm ấy đã mang lại thành công…
               </p>
-              <a className="text-yellow-500 inline-flex items-center mt-4">
-                Đọc thêm
-                <svg
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M5 12h14" />
-                  <path d="M12 5l7 7-7 7" />
-                </svg>
-              </a>
-            </div>
-          </div>
-          <div className="py-8 flex flex-wrap md:flex-nowrap">
-            <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <span className="font-semibold title-font text-gray-700">
-                CATEGORY
-              </span>
-              <span className="text-sm text-gray-500">12 Jun 2019</span>
-            </div>
-            <div className="md:flex-grow">
-              <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                Woke master cleanse drinking vinegar salvia
-              </h2>
-              <p className="leading-relaxed">
-                Glossier echo park pug, church-key sartorial biodiesel
-                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-                party messenger bag selfies, poke vaporware kombucha
-                lumbersexual pork belly polaroid hoodie portland craft beer.
-              </p>
-              <a className="text-yellow-500 inline-flex items-center mt-4">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://baobinhduong.vn/le-van-duong-ky-su-xay-dung-lam-nong-nghiep-cong-nghe-cao-a128477.html"
+                className="text-yellow-500 inline-flex items-center mt-4"
+              >
                 Đọc thêm
                 <svg
                   className="w-4 h-4 ml-2"
