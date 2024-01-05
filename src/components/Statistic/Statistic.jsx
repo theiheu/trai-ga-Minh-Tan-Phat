f02277cedad1630c1a723fd3d7c699bed4d05619
The Hieu <thehieu236@gmail.com>
2024-01-05 16:47:39 +0700

    Update image source path in Statistic component

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -8,7 +8,7 @@ const Statistic = () => {
           <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
             <div className="border-2 border-gray-200 px-4 py-6 rounded-lg">
               <img
-                src={require("../assets/statisticIcon/area.svg").default}
+                src={"../assets/statisticIcon/area.svg"}
                 className=" text-yellow-500 w-12 h-12 mb-3 inline-block"
                 alt=""
               />
